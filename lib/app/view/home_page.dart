0551f7793ef3740c8f7a18409e0f0f7e8ad7c9d9
Ulises M <30765968+lbux@users.noreply.github.com>
2024-04-18 19:10:14 -0700

    linking states of recommendations

--- lib/app/view/home_page.dart
+++ lib/app/view/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,8 +11,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentPageIndex = 0;
+  int selectedIndex = -1;
+
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
@@ -32,45 +38,66 @@ class _HomePageState extends State<HomePage> {
               itemCount: 5,
               itemBuilder: (context, index) {
                 final buttonText = (index.isEven) ? 'Agent' : 'Queue';
-                return Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListTile(
-                        title: Text(
-                          'Recommendation $index: Click to view details.',
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: FilledButton.tonal(
-                            onPressed: () {},
-                            child: Text(buttonText),
+                return InkWell(
+                  onTap: () {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+                  child: Card(
+                    color: selectedIndex == index
+                        ? colorScheme.secondaryContainer
+                        : colorScheme.background,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          title: Text(
+                            'Recommendation $index: Click to view details.',
                           ),
                         ),
-                      ),
-                    ],
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: FilledButton.tonal(
+                              onPressed: () {},
+                              child: Text(buttonText),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
             ),
           ),
-          const Expanded(
+          Expanded(
             child: Column(
               children: [
                 Expanded(
                   child: Card(
+                    color: colorScheme.secondaryContainer,
                     child: Center(
-                      child: Text('Recommendation Details'),
+                      child: Text(
+                        selectedIndex >= 0
+                            ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. $selectedIndex'
+                            : 'Recommendation Details',
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
                   child: Card(
+                    color: colorScheme.secondaryContainer,
                     child: Center(
-                      child: Text('Recommendation Charts'),
+                      child: Image.file(
+                        File(
+                          'temp.svg',
+                        ),
+                      ),
                     ),
                   ),
                 ),
