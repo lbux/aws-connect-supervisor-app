e1243752eb034535243bff0db8affbc170aab5bd
Ulises M <30765968+lbux@users.noreply.github.com>
2024-04-24 21:03:52 -0700

    finish mockup for presentation
    tried to use amazon colors, but they really suck
    
    adding some dummy text
    
    trying to fix text scaling but not 100% fixed

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -7,18 +7,31 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final myColorScheme = ColorScheme(
+      brightness: Brightness.light,
+      primary: const Color(0xFF146EB4),
+      onPrimary: Colors.white,
+      secondary: const Color(0xFFFF9900),
+      onSecondary: Colors.black,
+      error: Colors.redAccent,
+      onError: Colors.white,
+      background: const Color(0xFFF2F2F2),
+      onBackground: Colors.black,
+      surface: Colors.white,
+      onSurface: Colors.black,
+      primaryContainer: const Color(0xFF146EB4).withOpacity(0.8),
+      secondaryContainer: const Color(0xFFFF9900).withOpacity(0.8),
+    );
+    final themeData = ThemeData(
+      colorScheme: myColorScheme,
+    );
     final router = AppRouter.createRouter();
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        useMaterial3: true,
-      ),
+      theme: themeData,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
     );
--- lib/app/view/home_page.dart
+++ lib/app/view/home_page.dart
@@ -1,5 +1,23 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+List<String> recommendations = [
+  'Agent in Sales queue is struggling with slow response times. Additional training recommended.',
+  'Service level for Support queue is 5% below target. Consider taking action soon.',
+  'Agents in Support queue are idle. Recommend moving to Sales queue.',
+  'VIP queue experiencing an influx of calls. Recommend adding agents to the queue.',
+  'Agent in Sales consistently receives bad customer feedback. Review past interactions.',
+];
+
+List<String> inDepthRecommendations = [
+  'Agent Tom Harper in the Sales queue has an average response time of 30 seconds, significantly higher than the team average of 18 seconds. This slow response has been consistent over the past two weeks. To address this, a tailored training session focusing on efficient call handling and quick response techniques is recommended. This will likely help Tom decrease his response times to align with the team average.',
+  'The current service level in the Support queue is at 75%, which is below our target of 80%. Analysis from the last 48 hours shows that the queue often struggles during the 10 AM to 12 PM window. To rectify this, consider scheduling additional agents during this peak period or reviewing current call handling procedures to enhance efficiency and meet the target service level.',
+  'Data from the past week indicates that agents in the Support queue, such as Emily Johnson and Mark Lee, have a low contact rate with more than 50% idle time during their shifts. Meanwhile, the Sales queue has been experiencing a high volume of calls with a contacts-to-agent ratio of 3:1. Shifting Emily and Mark to the Sales queue could better utilize their capacity and balance the workload across queues.',
+  'The VIP queue has seen a 40% increase in call volume this month, with average wait times increasing to 25 seconds per call. Adding agents, such as Sarah Connor from the General queue, where call volumes have decreased by 20%, to the VIP queue could help manage this influx effectively and maintain our standard of service excellence for VIP customers.',
+  'Agent Jack Smith in the Sales queue has received consistently low customer satisfaction scores over his last 20 interactions, with particular complaints about resolution effectiveness and tone of communication. A detailed review of his past call recordings and customer feedback should be conducted to identify specific areas of improvement. Coaching or mentoring sessions should be scheduled based on the insights gained from the review.',
+];
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,16 +28,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentPageIndex = 0;
-  int selectedIndex = -1;
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
+    ScreenUtil.init(context);
 
     return Scaffold(
+      backgroundColor: colorScheme.background,
       appBar: AppBar(
         title: const Text('Home Page'),
         centerTitle: true,
+        backgroundColor: colorScheme.secondary,
         actions: [
           Padding(
             padding: const EdgeInsets.all(8),
@@ -33,43 +54,55 @@ class _HomePageState extends State<HomePage> {
       body: Row(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
+            child: Column(
+              children: List.generate(5, (index) {
                 final buttonText = (index.isEven) ? 'Agent' : 'Queue';
-                return InkWell(
-                  onTap: () {
-                    setState(() {
-                      selectedIndex = index;
-                    });
-                  },
+                return Expanded(
                   child: Card(
                     color: selectedIndex == index
-                        ? colorScheme.secondaryContainer
+                        ? colorScheme.primaryContainer
                         : colorScheme.background,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ListTile(
-                          title: Text(
-                            'Recommendation $index: Click to view details.',
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: FilledButton.tonal(
-                              onPressed: () {},
-                              child: Text(buttonText),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ListTile(
+                              title: AutoSizeText(
+                                recommendations[index],
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(fontSize: 24),
+                                minFontSize: 4,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              ),
                             ),
-                          ),
+                            const Spacer(),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: FilledButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    colorScheme.secondaryContainer,
+                                  ),
+                                ),
+                                child: Text(buttonText),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 );
-              },
+              }),
             ),
           ),
           Expanded(
@@ -77,20 +110,25 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Expanded(
                   child: Card(
-                    color: colorScheme.secondaryContainer,
+                    color: colorScheme.primaryContainer,
                     child: Center(
-                      child: Text(
-                        selectedIndex >= 0
-                            ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. $selectedIndex'
-                            : 'Recommendation Details',
-                        textAlign: TextAlign.center,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16, right: 16),
+                        child: AutoSizeText(
+                          textAlign: TextAlign.center,
+                          inDepthRecommendations[selectedIndex],
+                          style: const TextStyle(fontSize: 30),
+                          minFontSize: 8,
+                          maxLines: 8,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ),
                     ),
                   ),
                 ),
                 Expanded(
                   child: Card(
-                    color: colorScheme.secondaryContainer,
+                    color: colorScheme.primaryContainer,
                     child: Center(
                       child: SvgPicture.asset('temp.svg'),
                     ),
